6df77c14eafd4555d69b5dd8662aa6ff522e5bd1
TheSurfingCoder <matthewmctighe1@gmail.com>
2025-12-20 14:04:30 -0800

    chore: delete unused logger files and documentation

--- shared/logger.d.ts
+++
@@ -1,8 +0,0 @@
-import pino from 'pino';
-declare const logger: pino.Logger<never, boolean>;
-export declare const logEvent: (event: string, data?: Record<string, any>) => void;
-export declare const logError: (error: Error, context?: Record<string, any>) => void;
-export declare const logBusinessEvent: (event: string, data?: Record<string, any>) => void;
-export declare const logLifecycleEvent: (event: string, data?: Record<string, any>) => void;
-export default logger;
-//# sourceMappingURL=logger.d.ts.map
